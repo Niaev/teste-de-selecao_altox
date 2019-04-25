fc05453931657212219c6be7c81e193675d81baf
Tiago <trutnv@protonmail.com>
2019-04-25 16:40:24 -0300

    Teste #4 - Exibir imagem no HTML (falha)[C

--- index.py
+++ index.py
@@ -9,13 +9,19 @@ para: Teste de Seleção para vaga de Estagiário Python da Altox Lab
 from pathlib import Path
 from rdkit import Chem
 from rdkit.Chem import Draw
+from flask import Flask
 
-molecula = "O=C(C)Oc1ccccc1C(=O)O"
-im_dir = "./images/"
-im_type = ".png"
-im_path = im_dir + molecula + im_type
-imagem = Path(im_path)
+web_app = Flask(__name__)
 
-if imagem.exists() == False:
-    draw_im = Draw.MolToImage(Chem.MolFromSmiles(molecula))
-    draw_im.save(im_path)
+@web_app.route("/drawMolecule/<molecula>")
+def drawMolecule(molecula):
+    im_dir = "images/"
+    im_type = ".png"
+    im_path = im_dir + molecula + im_type
+    imagem = Path(im_path)
+    
+    if imagem.exists() == False:
+        draw_im = Draw.MolToImage(Chem.MolFromSmiles(molecula))
+        draw_im.save(im_path)
+    
+    return "<img src=\"" + im_path + "\">"
