76b9d2fdbbe1ec53d265ce86445becabff714440
Tiago <trutnv@protonmail.com>
2019-04-25 16:27:40 -0300

    Teste #3 - Salvando PIL Image

--- index.py
+++ index.py
@@ -6,8 +6,16 @@ para: Teste de Seleção para vaga de Estagiário Python da Altox Lab
 """
 
 # Imports
+from pathlib import Path
 from rdkit import Chem
 from rdkit.Chem import Draw
 
-# Teste
-print(Draw.MolToImage(Chem.MolFromSmiles("CCOCCNSC=O"))) # PIL.Image.Image
+molecula = "O=C(C)Oc1ccccc1C(=O)O"
+im_dir = "./images/"
+im_type = ".png"
+im_path = im_dir + molecula + im_type
+imagem = Path(im_path)
+
+if imagem.exists() == False:
+    draw_im = Draw.MolToImage(Chem.MolFromSmiles(molecula))
+    draw_im.save(im_path)
