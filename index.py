a73cd860cec748355f7da6d0add33b8c8c8d6dd1
Tiago <trutnv@protonmail.com>
2019-04-25 15:58:39 -0300

    Teste #1 - Chem.MolFromSmiles

--- index.py
+++ index.py
@@ -4,3 +4,9 @@ Simplified molecular-input line-entry system (SMILES)
 Desenvolvido por: Tiago Neiva Andrade
 para: Teste de Seleção para vaga de Estagiário Python da Altox Lab
 """
+
+# Imports
+from rdkit import Chem
+
+# Teste
+print(Chem.MolFromSmiles("CCOCCNSC=0"))
