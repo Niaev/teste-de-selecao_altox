ab861833d508d31617448f6197f9a771d34cb70c
Tiago <trutnv@protonmail.com>
2019-04-25 17:33:17 -0300

    Exibindo Imagem no HTML

--- index.py
+++ index.py
@@ -3,25 +3,35 @@ Simplified molecular-input line-entry system (SMILES)
 
 Desenvolvido por: Tiago Neiva Andrade
 para: Teste de Seleção para vaga de Estagiário Python da Altox Lab
+
+Fontes de apoio: https://stackoverflow.com/questions/31826335/how-to-convert-pil-image-image-object-to-base64-string
+                 http://flask.pocoo.org/docs/1.0/quickstart/#routing
 """
 
-# Imports
-from pathlib import Path
+# imports
+import base64
+from io import BytesIO
+
 from rdkit import Chem
 from rdkit.Chem import Draw
+
 from flask import Flask
 
+# instância flask - web app
 web_app = Flask(__name__)
 
+# rota /drawMolecule/
 @web_app.route("/drawMolecule/<molecula>")
 def drawMolecule(molecula):
-    im_dir = "images/"
-    im_type = ".png"
-    im_path = im_dir + molecula + im_type
-    imagem = Path(im_path)
     
-    if imagem.exists() == False:
-        draw_im = Draw.MolToImage(Chem.MolFromSmiles(molecula))
-        draw_im.save(im_path)
+    # buffer
+    bff = BytesIO()
     
-    return "<img src=\"" + im_path + "\">"
+    # desenha a imagem
+    draw_im = Draw.MolToImage(Chem.MolFromSmiles(molecula))
+    draw_im.save(bff, format="PNG")
+    
+    # base64 encode
+    im_to_b64 = base64.b64encode(bff.getvalue())
+    
+    return "<img src=\"data:image/png;base64, " + str(im_to_b64)[2:-1] + "\">"
